fe9cc9d627345459fd9cb6a9beb8dd16f9d24ea6
Otabek Hoshimxonov <otabekhoshimxon@gmail.com>
2024-06-09 09:52:07 +0500

    Update google-chrome.sh
    added docker last version install commands

--- google-chrome.sh
+++ google-chrome.sh
@@ -2,3 +2,18 @@ sudo apt-get install libxss1 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome*.deb
 sudo apt-get install -f
+
+
+
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
+sudo usermod -aG docker ${USER
+su - ${USER}
+id -nG
+sudo usermod -aG docker username
+docker run hello-world
